7f7c65a089f9ae3e1fe46d5ae032d8c21d046c78
ronglongchen <528438298@qq.com>
2023-06-06 22:57:16 +0800

    修改推送命令

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ go env -w GOOS=linux
 
 go build -o main ./main.go
 
-docker buildx build --platform linux/amd64 -t ronglongchen/build_demo .
+docker buildx build --platform linux/amd64 -t ronglongchen/build_demo . --push
 
 docker tag ronglongchen/build_demo ronglongchen/build_demo 
 
-docker push ronglongchen/build_demo 
+# docker push ronglongchen/build_demo 
 
 go env -w GOOS=darwin
