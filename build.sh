7e45513e7e80eac146a2765c93593c8f63efba21
ronglongchen <528438298@qq.com>
2023-06-06 23:37:19 +0800

    修改打包命令

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
 
 
-go env -w GOOS=linux
+# go env -w GOOS=linux
+
+docker rmi ronglongchen/build_demo && docker rm -f build_demo
 
 go build -o main ./main.go
 
-docker buildx build --platform linux/amd64 -t ronglongchen/build_demo . --push
+docker build -t ronglongchen/build_demo .
+
+# mac m1 打包出来服务器运行不了
+# docker buildx build --platform linux/amd64 -t ronglongchen/build_demo . --push
 
 docker tag ronglongchen/build_demo ronglongchen/build_demo 
 
-# docker push ronglongchen/build_demo 
+docker push ronglongchen/build_demo 
 
-go env -w GOOS=darwin
+
+docker run -d -p 9999 --name build_demo ronglongchen/build_demo 
+
+# go env -w GOOS=darwin
+
